8a5a612343afeb4116f921950c28baaf9fd9d586
Nguyen Linh Thanh Nhan <nguyenlinhthanhnhan@outlook.com>
2023-06-19 10:44:47 +0700

    Add Login page

---
+++ Athena.Blog.CMS/Models/LoginParamsType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Athena.Blog.CMS.Models;
+
+public class LoginParamsType
+{
+    [Required] public string UserName { get; set; }
+
+    [Required] public string Password { get; set; }
+
+    public bool AutoLogin { get; set; }
+}
---
+++ Athena.Blog.CMS/Pages/User/Login/Login.razor.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using AntDesign;
+using Athena.Blog.CMS.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Athena.Blog.CMS.Pages.User.Login;
+
+public partial class Login
+{
+    private readonly LoginParamsType _model = new LoginParamsType();
+
+    [Inject] public NavigationManager NavigationManager { get; set; }
+    
+    [Inject] public MessageService Message { get; set; }
+
+    public void HandleSubmit() {
+        if (_model.UserName == "admin" && _model.Password == "ant.design") {
+            NavigationManager.NavigateTo("/");
+            return;
+        }
+
+        if (_model.UserName == "user" && _model.Password == "ant.design") NavigationManager.NavigateTo("/");
+    }
+}
